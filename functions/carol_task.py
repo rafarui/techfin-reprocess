d34f7d1039ba55ef6b6c7f97bc871f471fe65615
rafarui <rafarui@gmail.com>
2021-04-07 15:08:19 -0300

    missing import

--- functions/carol_task.py
+++ functions/carol_task.py
@@ -492,7 +492,7 @@ def change_app_settings(login, app_name, settings, logger=None):
     return s
 
 
-def start_app_process(login, app_name, process_name):
+def start_app_process(login, app_name, process_name, logger=None):
     if logger is None:
         logger = logging.getLogger(login.domain)
 
